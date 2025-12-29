32ac85ad6e855be87c931572d4319bbc8e2ac8f6
ultragreed <agent002004@gmail.com>
2025-12-30 03:46:41 +1000

    Platform specific build

--- temp_kiosk.c
+++ temp_kiosk.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 
 int main(void) {
+#ifdef _WIN32
+    printf("All your codebase are belong to mini mild!\n");
+#else
     printf("All your codebase are belong to us!\n");
+#endif
     return 0;
 }
